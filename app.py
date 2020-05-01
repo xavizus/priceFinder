910ea7d862f6580a8e9a616e24296701565e56a5
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-05-01 23:53:46 +0200

    Added support for sending messages to discord. Added oldPrice for oldLowestPrice

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ import json
 import pymongo
 from datetime import datetime
 import config
+import requests
 productsToCheck = [
     {
         "productid": "ST4000NE001",
         "vendors": [
             {"vendor": "dustin",
-            "url": "https://www.dustinhome.se/product/5011164597/ironwolf-pro"},
+             "url": "https://www.dustinhome.se/product/5011164597/ironwolf-pro"},
             {"url": "https://www.elgiganten.se/product/datorer-tillbehor/harddisk-ssd-och-nas/149724/seagate-ironwolf-pro-3-5-intern-harddisk-for-nas-4-tb",
-            "vendor": "elgiganten"},
-            {"url":"https://www.inet.se/produkt/4301688/seagate-ironwolf-pro-4tb-7200rpm-256mb",
-            "vendor": "inet"},
+             "vendor": "elgiganten"},
+            {"url": "https://www.inet.se/produkt/4301688/seagate-ironwolf-pro-4tb-7200rpm-256mb",
+             "vendor": "inet"},
             {"url": "https://www.webhallen.com/api/product/312677",
-            "vendor": "webhallen"},
+             "vendor": "webhallen"},
             {"url": "https://www.komplett.se/product/1152689/datorutrustning/lagring/haarddisk/haarddisk-35/seagate-ironwolf-pro-4tb-nas-professional",
-            "vendor": "komplett"}
+             "vendor": "komplett"}
         ],
         "productName": "Seagate IronWolf-pro ST4000NE001 128MB 4TB",
         "size": 4
@@ -25,16 +26,16 @@ productsToCheck = [
         "productid": "ST6000VN0033",
         "vendors": [
             {"url": "https://www.dustinhome.se/product/5011168946/ironwolf-pro",
-            "vendor": "dustin"},
-            {"url":"https://www.inet.se/produkt/4301918/seagate-ironwolf-pro-6tb-7200rpm-256mb",
-            "vendor": "inet"},
+             "vendor": "dustin"},
+            {"url": "https://www.inet.se/produkt/4301918/seagate-ironwolf-pro-6tb-7200rpm-256mb",
+             "vendor": "inet"},
             {"url": "https://www.elgiganten.se/product/datorer-tillbehor/harddisk-ssd-och-nas/149723/seagate-ironwolf-pro-3-5-intern-harddisk-for-nas-6-tb",
-            "vendor": "elgiganten"},
+             "vendor": "elgiganten"},
             {"url": "https://www.webhallen.com/api/product/312678-Seagate",
-            "vendor": "webhallen"},
+             "vendor": "webhallen"},
             {"url": "https://www.komplett.se/product/1152685/datorutrustning/lagring/haarddisk/haarddisk-35/seagate-ironwolf-pro-6tb-nas-professional",
-            "vendor": "komplett"}
-            
+             "vendor": "komplett"}
+
         ],
         "productName": "Seagate IronWolf-pro ST6000VN0033 256MB 6TB",
         "size": 6
@@ -43,18 +44,18 @@ productsToCheck = [
         "productid": "ST8000VN004",
         "vendors": [
             {"url": "https://www.dustinhome.se/product/5011160398/ironwolf",
-            "vendor": "dustin"},
+             "vendor": "dustin"},
             {"url": "https://www.elgiganten.se/product/datorer-tillbehor/harddisk-ssd-och-nas/149739/seagate-ironwolf-pro-3-5-intern-harddisk-for-nas-8-tb",
-            "vendor": "elgiganten"},
-            {"url":"https://www.inet.se/produkt/4301448/seagate-ironwolf-8tb-7200rpm-256mb",
-            "vendor": "inet"},
+             "vendor": "elgiganten"},
+            {"url": "https://www.inet.se/produkt/4301448/seagate-ironwolf-8tb-7200rpm-256mb",
+             "vendor": "inet"},
             {"url": "https://www.webhallen.com/api/product/312089",
-            "vendor": "webhallen"},
+             "vendor": "webhallen"},
             {"url": "https://www.komplett.se/product/1147206/datorutrustning/lagring/haarddisk/haarddisk-35/seagate-ironwolf-8tb-35-nas-hdd",
-            "vendor": "komplett"},
+             "vendor": "komplett"},
             {"url": "https://www.netonnet.se/art/datorkomponenter/harddisk/sata-35-tum/seagate-ironwolf-st8000vn004-8tb/1009443.8991/",
-            "vendor": "netonnet"}
-            
+             "vendor": "netonnet"}
+
         ],
         "productName": "Seagate IronWolf ST8000VN004 256MB 8TB",
         "size": 8
@@ -63,41 +64,86 @@ productsToCheck = [
         "productid": "ST10000VN0008",
         "vendors": [
             {"url": "https://www.dustinhome.se/product/5011153178/ironwolf",
-            "vendor": "dustin"},
+             "vendor": "dustin"},
             {"url": "https://www.elgiganten.se/product/datorer-tillbehor/harddisk-ssd-och-nas/157258/seagate-ironwolf-3-5-intern-hdd-for-nas-10-tb",
-            "vendor": "elgiganten"},
-            {"url":"https://www.inet.se/produkt/4300258/seagate-ironwolf-4tb-5900rpm-64mb",
-            "vendor": "inet"},
+             "vendor": "elgiganten"},
+            {"url": "https://www.inet.se/produkt/4300258/seagate-ironwolf-4tb-5900rpm-64mb",
+             "vendor": "inet"},
             {"url": "https://www.webhallen.com/api/product/314439",
-            "vendor": "webhallen"},
+             "vendor": "webhallen"},
             {"url": "https://www.komplett.se/product/1146023/datorutrustning/lagring/haarddisk/haarddisk-35/seagate-ironwolf-10tb-35-nas-hdd",
-            "vendor": "komplett"},
+             "vendor": "komplett"},
             {"url": "https://www.netonnet.se/art/datorkomponenter/harddisk/sata-35-tum/seagate-ironwolf-st10000vn0008-10tb/1010198.8991/",
-            "vendor": "netonnet"}
+             "vendor": "netonnet"}
         ],
         "productName": "Seagate IronWolf ST10000VN0008 256MB 10TB",
         "size": 10
     }
 ]
 
+# Rate-Limit: 5 requests per 3 seconds
+def sendDiscordMessage(content):
+
+    content['oldPrice'] = content['oldPrice'] if content['oldPrice'] else "-"
+    if content['oldPrice'] == '-':
+        content['priceDiff'] = '-'
+    else:
+        content['priceDiff'] = (1-(content['price']/content['oldPrice']))*100
+    
+    payload = {
+        "file": "content",
+        "embeds": [
+            {
+                "title": f"Prices for hardrive with {content['size']} TB storage",
+                "fields": [
+                    {
+                        "name": "Product",
+                        "value": content['productName'],
+                        "inline": True
+                    },
+                    {
+                        "name": "Where",
+                        "value": content['vendor'],
+                        "inline": True
+                    },
+                    {
+                        "name": "Price",
+                        "value": content['price'],
+                        "inline:": True
+                    },
+                    {
+                        "name": "Old price",
+                        "value": content['oldPrice'],
+                        "inline": True
+                    },
+                    {
+                        "name": "Price Diffrence",
+                        "value": f"{content['priceDiff']} %",
+                    },
+                    {
+                        "name": "URL",
+                        "value": f"[{content['vendor']}]({content['url']})"
+                    }
+                ]
+            }
+        ]
+    }
+    if content['priceDiff'] == '-':
+        pass
+    elif content['priceDiff'] > 0:
+        payload['embeds'][0]['color'] = 2067276
+        payload['content'] = "A lower price have been found! <@&182749298390204416>"
+    elif content['priceDiff'] < 0:
+        payload['embeds'][0]['color'] = 10038562
+
+    response = requests.post(config.DISCORD_WEBHOOK, json=payload)
+    if response.status_code in [200, 204]:
+        print("Discord Message sent!")
+    else:
+        print(f"Something went wrong! Code: {response.status_code}")
+
+
 prices = priceFinder(productsToCheck)
 
-data = prices.getProducts()
-
-dbClient = pymongo.MongoClient(config.MONGODB)
-mydb = dbClient['pricefinder']
-myCol = mydb['products']
-collections = mydb.list_collection_names()
-
-if 'products' not in collections:
-    ids = myCol.insert_many(data)
-    print("Collection \"products\" is empty, filling with the current data. Nothing more to do.")
-    print(ids.inserted_ids)
-    raise SystemExit(0)
-
-for product in data:
-    for vendor in product['vendors']:
-        vendorDBData = myCol.find_one({'productid': product['productid'], "vendors.vendor": vendor['vendor']}, {"_id": 0, "vendors.$": 1})
-        vendorDBPrice = vendorDBData['vendors'][0]['price']
-        myCol.update_one({'productid': product['productid'], "vendors.vendor": vendor['vendor']}, { "$set": { "vendors.$.price": vendor['price'] }, "$push": {"vendors.$.oldPrices": {"date" : datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "price": vendorDBPrice}}})
-       
+for product in prices.lowestPrices:
+    sendDiscordMessage(product)
--- pricefinder.py
+++ pricefinder.py
@@ -3,17 +3,128 @@ from bs4 import BeautifulSoup as BS
 from typing import Type
 import re
 import utils
+import pymongo
+import config
+from datetime import datetime
 
 class priceFinder:
 
-    def __init__(self, products: dict):
-        self.products = products
+    def __init__(self, products: dict, testData=False):
+        self.products = testData or products
+        if not testData:
+            self.addPriceToProducts()
+        
+        dbClient = pymongo.MongoClient(config.MONGODB)
+        self.db = dbClient['pricefinder']
+        self.dbCol = self.db['products']
 
+
+        if not self.collectionExists('products'):
+            self.dbCol.insert_many(self.products)
+            print("Collection \"products\" is empty, filling with the current data.")
+        else:
+            self.updatePricesInDB()
+        self.lowestPrices = self.getLowestPricesForEachProduct()
+        self.setLowestPriceInDB()
+    
+    def setLowestPriceInDB(self):
+        for productIndex, product in enumerate(self.lowestPrices):
+            oldData = self.dbCol.find_one_and_update({"productid": product['productid']}, {"$set" : {"lowestPrice": product['price']}}, {"productid": 1, "lowestPrice": 1})
+            print(oldData)
+            if 'lowestPrice' in oldData:
+                self.lowestPrices[productIndex]['oldPrice'] = oldData['lowestPrice']
+            else:
+                self.lowestPrices[productIndex]['oldPrice'] = None
+
+    def getLowestPricesForEachProduct(self) -> list:
+        lowestPrices = []
+        # Credits to following site: 
+        # https://techbrij.com/mongodb-aggregation-query-records-max
+        for lowestPrice in self.dbCol.aggregate(
+            [
+	            {"$unwind": "$vendors"},
+	            {	
+		            "$group": {
+			            "_id": "$_id",
+			            "minPrice": {
+				            "$min": "$vendors.price"
+			            },
+			            "productGroup": {
+				            "$push": {
+					            "productid": "$productid",
+					            "productName": "$productName",
+					            "vendor": "$vendors.vendor",
+					            "price": "$vendors.price",
+					            "url": "$vendors.url",
+                                "size": "$size"
+				            }
+			            }
+		            }
+	            },
+	            {
+		            "$project": {
+			            "_id": 0,
+			            "toppers": {
+				            "$setDifference": [
+                                {
+					                "$map": {
+						                "input": "$productGroup",
+						                "as": "product",
+						                "in": {
+							                "$cond": [
+								                {
+									                "$eq": ["$minPrice", "$$product.price"]
+								                },
+								                "$$product",
+								                "false"
+							                ]
+						                }
+					                }
+				                },
+                                ["false"]
+                            ]
+			            }
+		            }
+	            },
+	            {
+		            "$unwind": "$toppers"
+	            },
+	            {
+		            "$project": {
+			            "productid": "$toppers.productid",
+			            "productName": "$toppers.productName",
+			            "vendor": "$toppers.vendor",
+			            "price": "$toppers.price",
+			            "url": "$toppers.url",
+                        "size": "$toppers.size"
+		            }
+	            }
+            ]
+        ):
+            lowestPrices.append(lowestPrice)
+
+        return lowestPrices
+
+    def collectionExists(self, collectionName) -> bool:
+        if collectionName in  self.db.list_collection_names():
+            return True
+        else:
+            return False
+
+    def updatePricesInDB(self):
+        for product in self.products:
+            for vendor in product['vendors']:
+                vendorDBData = self.dbCol.find_one({'productid': product['productid'], "vendors.vendor": vendor['vendor']}, {"_id": 0, "vendors.$": 1})
+                vendorDBPrice = vendorDBData['vendors'][0]['price']
+                self.dbCol.update_one({'productid': product['productid'], "vendors.vendor": vendor['vendor']}, { "$set": { "vendors.$.price": vendor['price'] }, "$push": {"vendors.$.oldPrices": {"date" : datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "price": vendorDBPrice}}})
+       
+
+    def addPriceToProducts(self):
         for productIndex, product in enumerate(self.products):
             for vendorIndex, vendor in enumerate(product['vendors']):
                 price = self.getCost(vendor['vendor'], vendor['url'])
                 self.products[productIndex]['vendors'][vendorIndex]['price'] = price
-    
+
     def getProducts(self):
         return self.products
     def getCost(self, vendor, url) -> int:
